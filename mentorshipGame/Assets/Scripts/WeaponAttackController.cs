68484d21204816be0ecff7f56393ec0d8d2e986b
Swiftfire7 <34386895+Swiftfire7@users.noreply.github.com>
2018-11-10 01:11:26 -0600

    started the combat system
    includes player movement and spawns a sword slash in front of the player in the direction of the cursor rotated correctly around the player with a cooldown,and shell code for character properties.

---
+++ mentorshipGame/Assets/Scripts/WeaponAttackController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAttackController : MonoBehaviour {
+
+    [SerializeField]
+    float timeToLive = 0.5f;//in seconds
+    public float attackDelay = 0.75f;
+    
+	
+	// Update is called once per frame
+	void Update () {
+        timeToLive -= Time.deltaTime;
+        if (timeToLive <= 0)
+            Destroy(gameObject);
+	}
+}
