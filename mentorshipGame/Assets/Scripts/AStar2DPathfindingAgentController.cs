d4496af0cfc7af7483c133199239e050b8cffb3d
Jeninuwin <Jenny.Nguyen@jacks.sdstate.edu>
2018-11-16 20:03:49 -0600

    Update mentorshipGame/Assets/Scripts/AStar2DPathfindingAgentController.cs

--- mentorshipGame/Assets/Scripts/AStar2DPathfindingAgentController.cs
+++ mentorshipGame/Assets/Scripts/AStar2DPathfindingAgentController.cs
@@ -1,24 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class AStar2DPathfindingAgentController : MonoBehaviour {
 
-
+    public GameObject TargetObject;
+    public TileMapController tileMapController;
     public TileMapSceneSupervisor tileMapSupervisor = null;
 
     private void Reset()
     {
         tileMapSupervisor = FindObjectOfType<TileMapSceneSupervisor>();
     }
+    private void Awake()
+    {
+        tileMapController = tileMapSupervisor.GetTileMapController(transform);
+    }
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Vector2 GotoPoint(Transform target)
+    {
+        Transform gridTransform = tileMapController.transform;
+        Debug.Assert(gridTransform.rotation == Quaternion.Euler(0, 0, 0), "Error: The tilemap should never be rotated!");
+
+        Vector3 tileSize = tileMapController.GetComponent<Tilemap>().cellSize;
+        /*
+
+        Vector2 nextPosition;
+        Vector2Int originTile = transform.position;
+        Vector2Int targetTile = target.position;
+
+        tileMapController.getNextTile(originTile, targetTile);
+        */
+        return new Vector2();
+    }
 }
