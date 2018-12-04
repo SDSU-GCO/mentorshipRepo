3e0e4fd8367328fa26e46473cea2798e5eee596e
Jeninuwin <Jenny.Nguyen@jacks.sdstate.edu>
2018-12-04 01:19:13 -0600

    Camera Systems

---
+++ mentorshipGame/Assets/Scripts/areacamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Cinemachine;
+
+public class areacamera : MonoBehaviour {
+    CinemachineVirtualCamera attachedareacam;
+    public static List<CinemachineVirtualCamera> cameralist = new List<CinemachineVirtualCamera>();
+    public static CinemachineVirtualCamera playercam = null;
+    private void OnDestroy()
+    {
+        if (cameralist != null && attachedareacam != null) 
+        {
+            cameralist.Remove(attachedareacam);
+        }
+    }
+    // Use this for initialization
+    void Awake () {
+        attachedareacam = GetComponent<CinemachineVirtualCamera>();
+        Debug.Assert(attachedareacam != null, "error attachedcamera null");
+        cameralist.Add(attachedareacam);
+
+	}
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        attachedareacam.enabled = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        attachedareacam.enabled = false;
+    }
+}
---
+++ mentorshipGame/Assets/Scripts/playercamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Cinemachine;
+
+public class playercamera : MonoBehaviour {
+    CinemachineVirtualCamera playercam;
+    
+	// Use this for initialization
+	void OnEnable () {
+        StartCoroutine("Moncam");
+        
+	}
+	IEnumerator Moncam()
+    {
+        do
+        {
+            yield return new WaitForEndOfFrame();
+            if (areacamera.cameralist != null)
+            {
+                bool camEn = true;
+                foreach (CinemachineVirtualCamera cvm in areacamera.cameralist)
+                {
+                    if (cvm.enabled == true)
+                        camEn = false;
+                }
+                playercam.enabled = camEn;
+            }
+        } while (enabled == true);
+    }
+	// Update is called once per frame
+	void Awake () {
+        playercam = GetComponent<CinemachineVirtualCamera>();
+        Debug.Assert(playercam != null, "error playercamera null");
+        playercam.enabled = true;
+        areacamera.playercam = playercam;
+    }
+}
