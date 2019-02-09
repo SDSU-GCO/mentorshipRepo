36ca6eef43da7a195d10f971edd2eb3d812ec5d2
Swiftfire7 <34386895+Swiftfire7@users.noreply.github.com>
2019-02-09 00:42:37 -0600

    Updated prefabs and eliminated unneccessarry code
    fixed the camera dolly, imported the UI canvas, tried to update the hearts, cut out some unused code that was commented out anyway, and fixed some animations.

--- mentorshipGame/Assets/Scripts/AllyLogic.cs
+++ mentorshipGame/Assets/Scripts/AllyLogic.cs
@@ -2,12 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using WeaponSupervisor;
+
 
 public class AllyLogic : MonoBehaviour
 {
-    //bool PlayerAttack = false; 
-    //bool SpAvailable = false;
     public WeaponAttackController weaponAttack;
     public bool partyLeader;
     public static AllyLogic partyLeaderObject;
@@ -31,7 +29,6 @@ public class AllyLogic : MonoBehaviour
     public float followDistanceMax;
     public float followDistanceMin;
     Rigidbody2D my2DRigidbody;
-    //List<AllyLogic> nearbyAllies = new List<AllyLogic>();
     Vector2 forcesFromAllies = new Vector2(0, 0);
     [SerializeField]
     float forceOfNearbyAllies = 1;
@@ -59,7 +56,6 @@ public class AllyLogic : MonoBehaviour
         AllyLogics.Remove(this);
     }
 
-    //AllyLogics.Add
     float rotationVariable = 0;
     public float rotationSpeed = 5;
     void Update ()
@@ -208,122 +204,27 @@ public class AllyLogic : MonoBehaviour
                 }
             }
         }
-
-        /*AllyLogic partyLeader = GetPartyLeader();
-        if (partyLeader != null)
-        {
-
-            float distance = Vector2.Distance(partyLeader.transform.position, transform.position);
-            if (canSeeTargetAlly(partyLeader))
-            {
-                if (distance < followDistanceMin)
-                {
-                    //back off
-                    my2DRigidbody.velocity = (((Vector2)transform.position) - ((Vector2)partyLeader.transform.position)).normalized * moveSpeed;
-                }
-                else if (distance < followDistanceMax)
-                {
-                    //stay still
-                }
-                else
-                {
-                    //move closer
-                    my2DRigidbody.velocity = (((Vector2)partyLeader.transform.position) - ((Vector2)transform.position)).normalized * moveSpeed;
-                }
-            }
-            else
-            {
-                //path to ally
-                Vector2 nextPoint = getNextPoint;
-                my2DRigidbody.velocity = (nextPoint - (Vector2)transform.position).normalized * moveSpeed;
-            }
-        }
     }
-
-    getNearbyAllies();
-    foreach(AllyLogic ally in AllyLogics)
-    {
-        Vector2 tempForce = new Vector2(forceOfNearbyAllies, forceOfNearbyAllies);
-        tempForce.Scale( ((Vector2)transform.position - (Vector2)ally.transform.position));
-        forcesFromAllies += tempForce;
-    }*/
-
-        //getComponent.characterManager.Character.SpecialAttack;
-
-        /**if (SpAvailable == true && Input.GetKeyDown("space"))
-        {
-            fireSpecial();
-        }
-
-        bool superfriendshipbeam = false;
-        bool PhilSpecial = getComponent.characterManager.Philif.SpecialAttack;
-        bool FelSpecial = getComponent.characterManager.Felicia.SpecialAttack;
-        bool VerSpecial = getComponent.characterManager.Veronica.SpecialAttack;
-        bool TaySpecial = getComponent.characterManager.Taylor.SpecialAttack;
-    
-        foreach (AllyLogic allyLogic in AllyLogic)
-        {
-            SuperFriendshipBeam();
-        }
-        **/
-    }
-
-    /*private void getNearbyAllies()
-    {
-        nearbyAllies.Clear();
-        foreach(AllyLogic ally in AllyLogics)
-        {
-            if(ally!=this && Vector2.Distance(transform.position, ally.transform.position) <= repulsionDistance)
-            {
-                nearbyAllies.Add(ally);
-            }
-        }
-        return;
-    */
 
     public float offset = 1.5f;
-    //GameObject gameObjectTarget = new GameObject();
     CircleCollider2D circleCollider2D = null;
 
     [SerializeField]
     float repulsionDistance;
 
-    /*private bool canSeeTargetAlly(AllyLogic ally)
+    public static AllyLogic  GetPartyLeaderGlobal()
     {
-        bool canSeeTarget = false;
-        gameObjectTarget.SetActive(true);
-        
-        CircleCollider2D temp = gameObjectTarget.GetComponent<CircleCollider2D>();
-        if (temp == null)
-            circleCollider2D = gameObjectTarget.AddComponent<CircleCollider2D>();
-        else
-            circleCollider2D = temp;
-        
-        circleCollider2D.enabled = true;
-        circleCollider2D.radius = 0.05f;
-        circleCollider2D.offset = offset;
 
-        gameObjectTarget.transform.position = ally.transform.position;
-        gameObjectTarget.transform.rotation = ally.transform.rotation;
-
-        LayerMask layerMask = LayerMask.NameToLayer("Enemy");
-        layerMask |= ally.gameObject.layer;
-        layerMask = ~layerMask;
-
-        RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position, (Vector2)ally.transform.position - (Vector2)transform.position, Mathf.Infinity, layerMask);
-        if( raycastHit2D.collider == circleCollider2D)
+        AllyLogic partylead = null;
+        foreach (AllyLogic allyLogic in AllyLogics)
         {
-            canSeeTarget = true;
+            if (allyLogic.partyLeader == true)
+                partylead = allyLogic;
         }
+        return partylead;
+    }
 
-
-        circleCollider2D.enabled = false;
-        gameObjectTarget.SetActive(false);
-
-        return canSeeTarget;
-    }*/
-
-    AllyLogic GetPartyLeader()
+    public AllyLogic GetPartyLeader()
     {
         
         AllyLogic partylead = null;
@@ -334,11 +235,6 @@ public class AllyLogic : MonoBehaviour
         }
         return partylead;
     }
-
-    /**void superFriendshipBeam()
-    {
-        //Object.Instantiate(("assets/SFB"), transform.position, transform.rotation);
-    }**/
 
     void FireAttack()
     {
@@ -384,13 +280,7 @@ public class AllyLogic : MonoBehaviour
             GameObject childInstance = Instantiate(rangedAttack.gameObject, mouseposition + (Vector2)transform.position, transform.rotation);
             childInstance.GetComponent<Rigidbody2D>().velocity = rangedAttack.speed * mouseposition.normalized;
 
-            //GameObject childInstance = Instantiate(Square, shoot, Quaternion.FromToRotation(transform.position, shoot));
-
-
             rangedCoolDownInSeconds = rangedCoolDownInSecondsDefault;
-
-            //GameObject Target = Instantiate(Square, mouseposition + (Vector2)transform.position, transform.rotation);
-            //rangedCoolDownInSeconds = rangedCoolDownInSecondsDefault;
                 
         }
         
@@ -401,5 +291,6 @@ public class AllyLogic : MonoBehaviour
         playerHealth -= amount;
         if (playerHealth <= 0 && partyLeader == true)
             Debug.Log("Game Over");
+        HeartContainers.hearts.UpdateHP();
     }
 }
---
+++ mentorshipGame/Assets/Scripts/HeartContainers.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AllyLogic))]
+public class HeartContainers : MonoBehaviour
+{
+    public List<GameObject> heartContainers;
+
+    public static HeartContainers hearts = null;
+
+    private static AllyLogic allyLogicRef;
+
+    private void Awake()
+    {
+        hearts = this;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        allyLogicRef = AllyLogic.GetPartyLeaderGlobal();
+        heartContainers.Add(gameObject);
+        Debug.Log(heartContainers.Count);
+    }
+    /// <summary>
+    /// gets a reference to playerhealth and updates the heart containers/game over accordingly
+    /// </summary>
+    public void UpdateHP()
+    {
+        Debug.Log(heartContainers.Count);
+        for (int i = 0; i < heartContainers.Count; i++)
+        {
+            if (i <= allyLogicRef.GetPartyLeader().playerHealth)
+            {
+                heartContainers[i].GetComponent<Animator>().enabled = true;
+                Debug.Log(true);
+            }
+            else
+            {
+                heartContainers[i].GetComponent<Animator>().enabled = false;
+                Debug.Log(false);
+            }
+        }
+
+    }
+}
