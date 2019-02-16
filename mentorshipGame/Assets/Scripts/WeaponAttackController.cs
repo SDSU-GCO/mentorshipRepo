255baf30b4a686eb87d52fc37aab1389dc242434
Swiftfire7 <34386895+Swiftfire7@users.noreply.github.com>
2019-02-16 01:19:29 -0600

    got life working and implemented healing
    animated the hearts and added healing apples

--- mentorshipGame/Assets/Scripts/WeaponAttackController.cs
+++ mentorshipGame/Assets/Scripts/WeaponAttackController.cs
@@ -6,25 +6,28 @@ public class WeaponAttackController : MonoBehaviour {
 
     [SerializeField]
     float timeToLive = 0.5f;//in seconds
+    [SerializeField]
+    private bool LiveForever = false;
     float originalTimeToLive;//in seconds
     public float AttackDelay = 0.75f;
     public bool attackingFriendlies = false;
     public int damage;
     public float speed = 7;
     new Collider collider = null;
+    
 
     private void Awake()
     {
         originalTimeToLive = timeToLive;
         collider = GetComponent<Collider>();
-        if (collider!=null && collider.isTrigger == false)
-            collider = null;
+        if (collider!=null && collider.isTrigger == false && LiveForever == false)
+            collider.enabled = false;
     }
 
     // Update is called once per frame
     void Update () {
-        
-        timeToLive -= Time.deltaTime;
+        if (LiveForever != true)
+            timeToLive -= Time.deltaTime;
         if(collider != null && timeToLive< originalTimeToLive/2.0f)
         {
             collider.enabled = true;
@@ -54,11 +57,15 @@ public class WeaponAttackController : MonoBehaviour {
         if (collision.tag == "ally" && attackingFriendlies == true)
         {
             collision.gameObject.GetComponent<AllyLogic>().TakeDamage(damage);
+           if (LiveForever == true)
+            Destroy(gameObject);
         }
         else if (collision.tag == "enemy" && attackingFriendlies == false)
         {
             collision.gameObject.GetComponent<EnemyLogic>().TakeDamage(damage);
+            
         }
+        
     }
 
 }
