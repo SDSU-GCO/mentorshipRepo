27a50b1a52adae24ae44aa224e832a98ac116378
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-02-17 23:03:01 -0600

    Imported Jenny's cutscene system

---
+++ mentorshipGame/Assets/Scripts/Cutscene Scripts/ImageController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class ImageController : MonoBehaviour 
+{
+    public float fadeTime = 1f;
+    public GameObject nextImage = null;
+    public GameObject prevImage = null;
+    private GameObject oldImage = null;
+
+    Color color = new Color();
+
+    float opacityValue = 0.0f;
+    bool fadeFlag = false;
+    SpriteRenderer BGrenderer;
+
+    private void Awake()
+    {
+        BGrenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(StartFade());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("RightArrow") && opacityValue == 1f)
+        {
+            NextImage();
+        }
+
+        if (Input.GetKeyDown("LeftArrow") && opacityValue == 1f)
+        {
+            PrevImage();
+        }
+    }
+
+    protected virtual void NextImage()
+    {
+        Vector3 spwanposition = transform.position;
+        spwanposition.z = spwanposition.z - 0.01f;
+        if (nextImage == null)
+            Debug.LogError("ya fuked up");
+        GameObject newObject = Instantiate(nextImage, spwanposition, Quaternion.Euler(Vector3.zero));
+        newObject.transform.parent = transform.parent;
+        newObject.GetComponent<ImageController>().oldImage = gameObject;
+    }
+
+    void PrevImage()
+    {
+        Vector3 spwanposition = transform.position;
+        spwanposition.z = spwanposition.z - 0.01f;
+        if (prevImage == null)
+            Debug.LogError("ya fuked up");
+        GameObject newObject = Instantiate(prevImage, spwanposition, Quaternion.Euler(Vector3.zero));
+        newObject.GetComponent<ImageController>().oldImage = gameObject;
+    }
+
+    IEnumerator StartFade()
+    {
+        color = BGrenderer.color;
+        while (opacityValue < 1)
+        {
+            opacityValue += Time.deltaTime / fadeTime;
+            opacityValue = Mathf.Min(1, opacityValue);
+
+            color.a = opacityValue;
+            BGrenderer.color = color;
+            yield return null;
+        }
+        if (oldImage != null)
+            Destroy(oldImage);
+    }
+}
---
+++ mentorshipGame/Assets/Scripts/Cutscene Scripts/ImageSupervisor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageSupervisor : MonoBehaviour {
+
+    public GameObject firstimage;
+    public Transform rootObject;
+
+	// Use this for initialization
+	void Start () {
+        Vector3 spwanposition = transform.position;
+        spwanposition.z = spwanposition.z - 0.01f;
+        if (firstimage == null)
+            Debug.LogError("ya fuked up");
+        GameObject go = Instantiate(firstimage, spwanposition, Quaternion.Euler(Vector3.zero));
+        go.transform.parent = rootObject;
+	}
+	
+}
